94412a63a6bec55d1d5829d8a4174be36eeaadd6
UliDantas <dantas.ulisses@hotmail.com>
2025-02-05 15:37:50 -0300

    feature: added disable gameobject option to scale transition

--- Runtime/Transitions/ScaleTransition.cs
+++ Runtime/Transitions/ScaleTransition.cs
@@ -8,12 +8,16 @@ namespace Uli.Transition
         [Header("0 = Out, 1 = IN")]
         public Vector3[] scaleRefs = new Vector3[2] { Vector3.zero, Vector3.one };
 
+        public bool disableGOWhenDone = false;
+
         public override void SetState(bool doEnable)
         {
             if (hasEntered == doEnable)
                 return;
 
             base.SetState(doEnable);
+
+            EnableObject(doEnable);
 
             if (doEnable)
                 objectToTween.transform.localScale = scaleRefs[1];
@@ -28,13 +32,28 @@ namespace Uli.Transition
             base.DoTween(doEnable);
 
             if (doEnable)
+            {
+                EnableObject(true);
                 objectToTween.DOScale(scaleRefs[1], movementTime).SetEase(tweenType).SetDelay(enterDelay).SetUpdate(true);
+            }
             else
-                objectToTween.DOScale(scaleRefs[0], movementTime).SetEase(tweenType).SetDelay(exitDelay).SetUpdate(true);
+            {
+                objectToTween.DOScale(scaleRefs[0], movementTime).SetEase(tweenType).SetDelay(exitDelay).SetUpdate(true).OnComplete(() =>
+                {
+                    //Extra check in case of simultaneously cancelling(killing) the current fade-out and doing a fade-in
+                    if (hasEntered == false)
+                        EnableObject(false);
+                });
+            }
         }
         public override void ToggleState()
         {
             DoTween(!hasEntered);
+        }
+        private void EnableObject(bool doEnable)
+        {
+            if (disableGOWhenDone)
+                objectToTween.gameObject.SetActive(doEnable);
         }
     }
 }
